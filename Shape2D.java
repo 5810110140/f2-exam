0d6c36a8b5b603b00f68c3974ffafa8383872918
ThaninLimkulpong <5810110140@psu.ac.th>
2020-05-09 13:21:28 +0700

    git use start

---
+++ Shape2D.java
@@ -0,0 +1,6 @@
+public abstract class Shape2D extends Shape implements AreaCalculable {
+    public Shape2D(String name){
+        super(name);
+    }
+    
+}
---
+++ Shape3D.java
@@ -0,0 +1,6 @@
+public abstract class Shape3D extends Shape implements AreaCalculable,VolumeCalculable {
+    public Shape3D(String name){
+        super(name);
+    }
+    
+}
